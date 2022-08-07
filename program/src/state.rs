365c3ff6ea4e0f203e50944d703cea9818a0fb20
David Zarandi <david.zarandi@pm.me>
2022-08-07 18:56:24 +0200

    Fixed is_initialized

--- program/src/state.rs
+++ program/src/state.rs
@@ -2,6 +2,14 @@ use solana_program::{ pubkey::Pubkey, program_pack::{ IsInitialized, Sealed } };
 use borsh::{ BorshSerialize, BorshDeserialize };
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct State {}
+pub struct State {
+  pub is_initialized: bool,
+}
 
 impl Sealed for State {}
+
+impl IsInitialized for State {
+  fn is_initialized(&self) -> bool {
+    self.is_initialized
+  }
+}
